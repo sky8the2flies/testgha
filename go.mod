084b0213f2e7f232e6ff92fae0017f9d565b0e55
sky <skylerseifert@gmail.com>
2024-11-11 12:33:52 -0800

    feat: test reading from serial config

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.23
 require (
 	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/go-resty/resty/v2 v2.15.3
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 )
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,81 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"swift-hub-app/api"
-	"swift-hub-app/config"
-	"swift-hub-app/mqtt"
-	"syscall"
+	"time"
+
+	"github.com/tarm/serial"
 )
 
 func main() {
-	log.Print("Starting")
-	// Load configuration
-	cfg := config.LoadConfig()
+	// Configure serial connection settings
+	config := &serial.Config{
+		Name:        "/dev/ttyAMA0",
+		Baud:        38400,
+		Size:        8,
+		StopBits:    serial.Stop1,
+		Parity:      serial.ParityNone,
+		ReadTimeout: time.Second * 5,
+	}
 
-	// Initialize API client
-	apiClient := api.NewClient(cfg)
+	// Open serial port
+	port, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatalf("Failed to open port: %v", err)
+	}
+	defer port.Close()
 
-	// Initialize MQTT client
-	mqttClient := mqtt.InitMQTTClient(cfg, mqtt.MessageHandler(apiClient))
+	// Buffer for reading data
+	buffer := make([]byte, 128)
 
-	// Subscribe to temperature topics
-	mqtt.SubscribeToTemperature(mqttClient)
+	fmt.Println("Reading from serial port...")
+	for {
+		n, err := port.Read(buffer)
+		if err != nil {
+			log.Printf("Error reading from serial port: %v", err)
+			continue
+		}
 
-	// Set up signal handling for graceful shutdown
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	<-sigs // Wait for termination signal
-
-	log.Println("Shutting down...")
-	mqttClient.Disconnect(250)
+		if n > 0 {
+			// Print received data
+			fmt.Printf("Received data: %s\n", buffer[:n])
+		}
+	}
 }
+
+// package main
+
+// import (
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"swift-hub-app/api"
+// 	"swift-hub-app/config"
+// 	"swift-hub-app/mqtt"
+// 	"syscall"
+// )
+
+// func main() {
+// 	log.Print("Starting")
+// 	// Load configuration
+// 	cfg := config.LoadConfig()
+
+// 	// Initialize API client
+// 	apiClient := api.NewClient(cfg)
+
+// 	// Initialize MQTT client
+// 	mqttClient := mqtt.InitMQTTClient(cfg, mqtt.MessageHandler(apiClient))
+
+// 	// Subscribe to temperature topics
+// 	mqtt.SubscribeToTemperature(mqttClient)
+
+// 	// Set up signal handling for graceful shutdown
+// 	sigs := make(chan os.Signal, 1)
+// 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+// 	<-sigs // Wait for termination signal
+
+// 	log.Println("Shutting down...")
+// 	mqttClient.Disconnect(250)
+// }
